d2bdc36f7095d38c5df561e87c9f78c7bec23dff
Dretse <pub3@thebaud.eu>
2017-11-06 08:35:10 +0100

    ajout des commentaires dans couleur

--- src/Couleur.java
+++ src/Couleur.java
@@ -1,34 +1,36 @@
 
 public class Couleur {
-	int R,G,B;
+	int R,V,B;
 	int N=1;
 	// N représente de combien de Couleurs est issue celle ci, 
 	//pour pouvoir faire des moyennes sans consommer trop de mémoire 
 	public Couleur(int Red, int Green, int Blue)
 	{
+		//Fonction constructeur avec 3 entiers
 		R=Red;
-		G=Green;
+		V=Green;
 		B=Blue;
 	}
 	public Couleur(int[] RVB)
 	{
-		// deuxième constructeur pour faciliter la vie
+		// deuxième constructeur pour construire à partir d'un tableau de int
 		R=RVB[0];
-		G=RVB[1];
+		V=RVB[1];
 		B=RVB[2];
 	}
 	
-	public void initN(int n)
+	private void initN(int n)
 	{
+		//pour changer la valeur de n, utilisée uniquement par la fonction moyenne
 		N=n;
 	}
 	public int getR()
 	{
 		return R;
 	}
-	public int getG()
+	public int getV()
 	{
-		return G;
+		return V;
 	}
 	public int getB()
 	{
@@ -40,7 +42,7 @@ public class Couleur {
 	}
 	public int[] getRVB()
 	{
-		int[] a= {R,G,B};
+		int[] a= {R,V,B};
 		return a;
 	}
 	
@@ -48,21 +50,25 @@ public class Couleur {
 	{
 		//calcule la distance au carré entre 2 Couleurs, sur les 3 axes
 		int A=(R-C.getR())*(R-C.getR());
-		A+=(G-C.getG())*(G-C.getG());
+		A+=(V-C.getV())*(V-C.getV());
 		A+=(B-C.getB())*(B-C.getB());
 		return A;
 	}
 	
 	public void printcoul()
 	{
-		System.out.printf(" %d , %d , %d \n",R, G, B);
+		// affiche les 3 coordonnées d'une couleur
+		//uniquement à but de test
+		System.out.printf(" %d , %d , %d \n",R, V, B);
 	}
 	
 	public Couleur moyenne(Couleur C)
 	{
+		//Calcule la moyenne entre 2 couleurs, renvoie une nouvelle couleur,
+		//contenant l'information dans N du nombre de couleurs dont elle est issue
 		int[] A=new int[3];
 		A[0]=(R*N + C.getR()*C.getN())/(N+C.getN());
-		A[1]=(G*N + C.getG()*C.getN())/(N+C.getN());
+		A[1]=(V*N + C.getV()*C.getN())/(N+C.getN());
 		A[2]=(B*N + C.getB()*C.getN())/(N+C.getN());
 		Couleur Retour=new Couleur(A);
 		Retour.initN(N+C.getN());
@@ -70,7 +76,45 @@ public class Couleur {
 	}
 	public boolean egal(Couleur C)
 	{
-		return(R==C.getR() && G==C.getG() && B==C.getB());
+		// renvoie un booleen pour savoir les 2 couleurs sont égales
+		return(R==C.getR() && V==C.getV() && B==C.getB());
+	}
+	public int comparaison(int direction, Couleur col){
+		//compare 2 couleurs suivan une direction: renvoie 1 si la premiere est plus grande,
+		//0 si elles sont égales
+		//-1 si la premiere est plus petite
+		switch (direction){
+		default: //case 0, par defaut la racine
+			if (this.getR()>col.getR()){
+				return 1;
+			}
+			else if(this.getR()==col.getR()){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		case 1:
+			if (this.getV()>col.getV()){
+				return 1;
+			}
+			else if(this.getV()==col.getV()){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		case 2:
+			if (this.getB()>col.getB()){
+				return 1;
+			}
+			else if(this.getB()==col.getB()){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		}
 	}
 	
 }
--- src/KdNode.java
+++ src/KdNode.java
@@ -5,39 +5,42 @@ public class KdNode {
 	Couleur coul;
 	KdNode filsG;
 	KdNode filsD;
-	boolean[] fils= {false,false};
 	public KdNode(Couleur C, int p)
 	{
 		//Constructeur : P=profondeur du noeud, coul= Couleur
 		P=p;
 		coul=C;
 	}
-	
 	public int addpointnode(Couleur RVB)
 	{
 		//ajoute un point par recursivite et renvoie la profondeur a laquelle il a �t� ajout�
+		if(RVB.getRVB()[P%3]==coul.getRVB()[P%3])
+		{
+			if(RVB.egal(coul))
+			{
+				return 0;
+			}
+		}
 		if (RVB.getRVB()[P%3]>coul.getRVB()[P%3])
 		{
-			if(fils[1])
+			if(filsD!=null)
 			{
 				return(filsD.addpointnode(RVB));
 			}
 			else
 			{
-				fils[1]=true;
 				filsD= new KdNode(RVB, P+1);
 				return P+1;
 			}
 		}
 		else
 		{
-			if(fils[0])
+			if(filsG!=null)
 			{
 				return(filsG.addpointnode(RVB));
 			}
 			else
 			{
-				fils[0]=true;
 				filsG= new KdNode(RVB, P+1);
 				return P+1;
 			}
@@ -61,14 +64,14 @@ public class KdNode {
 			int i;
 			Couleur[] A=new Couleur[L/2];
 			Couleur[] B=new Couleur[L/2];
-			if(fils[0]) {A=filsG.palnode(A);}
+			if(filsG!=null) {A=filsG.palnode(A);}
 			else {
 				for(i=0;i<L/2;i++)
 				{
 					A[i]=new Couleur(0,0,0);
 				}
 			}
-			if(fils[1]) {B=filsD.palnode(B);}
+			if(filsD!=null) {B=filsD.palnode(B);}
 			else
 			{
 				for(i=0;i<L/2;i++)
@@ -87,18 +90,18 @@ public class KdNode {
 	}
 	public Couleur Moy()
 	{
-		if(fils[0] && fils[1])
+		if(filsG!=null && filsD!=null)
 		{
 			Couleur A = (filsD.Moy()).moyenne(coul);
 			A=(filsG.Moy()).moyenne(A);
 			return A;
 		}
-		else if(fils[1])
+		else if(filsD!=null)
 		{
 			Couleur A = (filsD.Moy()).moyenne(coul);
 			return A;
 		}
-		else if(fils[0])
+		else if(filsG!=null)
 		{
 			Couleur A = (filsG.Moy()).moyenne(coul);
 			return A;
@@ -108,19 +111,18 @@ public class KdNode {
 			return coul;
 		}
 	}
-
 	public Couleur getNN(Couleur A)
 	{
 		if ((A.getRVB())[P%3] > (coul.getRVB())[P%3])
 		{
-			if (fils[1])
+			if (filsD!=null)
 			{
 				Couleur CurrentBest = filsD.getNN(A);
 				if (CurrentBest.distance(A) > coul.distance(A))
 				{
 					CurrentBest=coul;
 				}
-				if(CurrentBest.distance(A) > Math.abs((coul.getRVB())[P%3] - (A.getRVB())[P%3])  && fils[0])
+				if(CurrentBest.distance(A) > Math.abs((coul.getRVB())[P%3] - (A.getRVB())[P%3])  && filsG!=null)
 				{
 					Couleur Alter = filsG.getNN(A);
 					if(CurrentBest.distance(A) > Alter.distance(A))
@@ -137,14 +139,14 @@ public class KdNode {
 		}
 		else
 		{
-			if (fils[0])
+			if (filsG!=null)
 			{
 				Couleur CurrentBest = filsG.getNN(A);
 				if (CurrentBest.distance(A) > coul.distance(A))
 				{
 					CurrentBest=coul;
 				}
-				if(CurrentBest.distance(A) > Math.abs((coul.getRVB())[P%3] - (A.getRVB())[P%3])  && fils[1])
+				if(CurrentBest.distance(A) > Math.abs((coul.getRVB())[P%3] - (A.getRVB())[P%3])  && filsD!=null)
 				{
 					Couleur Alter = filsD.getNN(A);
 					if(CurrentBest.distance(A) > Alter.distance(A))
