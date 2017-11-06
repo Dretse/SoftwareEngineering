89b1f94a4f34518142e5fa1980232c62948d1775
Dretse <pub3@thebaud.eu>
2017-11-06 08:40:44 +0100

    ajout des commentaires dans KdTree

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,18 +1,19 @@
 
 public class KdTree{
 	private int profondeur=0;
-
-	private Couleur CouleurR;
 	private KdNode Racine;
 
 	public KdTree (int RVB[]){
-		this.CouleurR=new Couleur(RVB[0], RVB[1], RVB[2]);
+		//fonction construceur qui crée le noeud racine de l'arbre (profondeur = 0)
+		Couleur CouleurR=new Couleur(RVB[0], RVB[1], RVB[2]);
 		this.Racine= new KdNode(CouleurR,0);
 	}
 	
 
 
 	public void addpoint(int RVB[]){
+		// ajout un point à l'arbre en le donnant à la racine.
+		//actualise la profondeur de l'arbre
 		Couleur C=new Couleur(RVB);
 		int p= Racine.addpointnode(C);
 		if(p>profondeur)
@@ -24,6 +25,7 @@ public class KdTree{
 	public Couleur[] palette(int pow)
 	{
 		//renvoie une palette de 2**pow Couleurs
+		//représentative de l'arbre
 		Couleur[] palette= new Couleur[(int) Math.pow(2, pow-1)];
 		palette= Racine.palnode(palette);
 		return palette;
@@ -31,6 +33,8 @@ public class KdTree{
 	
 	public int getNearestNeighbors(Couleur A, Couleur[] pal)
 	{
+		//Trouve le plus proche voisin dans l'arbre d'une couleur A donnée,
+		//parmis les couleurs comprises dans une palette donnée
 		Couleur Nearest= Racine.getNN(A);
 		int i;
 		for(i=0;i<pal.length;i++)
